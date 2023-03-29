d819d28e1d3288c73e4a8f4045ee19b32e2e2cb5
Oğuz Evrensel <oevrensel@gmail.com>
2023-03-29 15:27:50 +0300

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Fake;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+        private List<User> _users = FakeData.GetUsers(200);
+        public List<User> Get()
+        {
+            return _users;
+        }
+
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+           
+            return _users.FirstOrDefault(u => u.Id == id);
+        }
+
+        [HttpPost]
+        public User Post([FromBody]User user)
+        {
+            _users.Add(user);
+            return user;
+        }
+
+        [HttpPut]
+        public User Put([FromBody] User user)
+        {
+            var _user = _users.FirstOrDefault(a => a.Id == user.Id);
+            if(_user != null)
+            {
+                _user.Id = user.Id;
+                _user.FirstName = user.FirstName;
+                _user.LastName = user.LastName;
+                _user.Address = user.Address;
+            }
+            return user;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var _user = _users.FirstOrDefault(a => a.Id == id);
+            _users.Remove(_user);
+
+
+        }
+
+    }
+
+}
+
---
+++ Fake/FakeData.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+using WebApi.Models;
+
+namespace WebApi.Fake
+{
+    public class FakeData
+    {
+        public static List<User> _Users;
+
+        public static List<User> GetUsers(int number)
+        {
+            if(_Users == null)
+            {
+                _Users = new Faker<User>().RuleFor(u => u.Id, f => f.IndexFaker + 1)
+                .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+                .RuleFor(u => u.LastName, f => f.Name.LastName())
+                .RuleFor(u => u.Address, f => f.Address.FullAddress())
+                .Generate(number);
+
+            }
+            return _Users;
+        }
+
+
+    }
+}
+
+
+
+
+
+
